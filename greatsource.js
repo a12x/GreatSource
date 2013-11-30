7321cf0410b51b25adcbe81332edf6c67ff2e816
a12x <a12xyzw@gmail.com>
2013-11-30 01:31:32 -0800

    can get course and term from a page

--- greatsource.js
+++ greatsource.js
@@ -1,1 +1,14 @@
-alert("Hello, World!");
+var getTitle = function() {
+	return $('table:eq(2) b').text();
+}
+
+var getCourse = function() {
+  return getTitle().split('-')[0].trim();
+}
+
+var getTerm = function() {
+  return getTitle().split('-')[1].trim();
+}
+
+console.log(getCourse());
+console.log(getTerm());
