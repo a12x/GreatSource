58986ecd60626cec29648dd5036154dfb097308c
a12x <a12xyzw@gmail.com>
2013-11-30 02:02:13 -0800

    highlight a secret number

--- greatsource.js
+++ greatsource.js
@@ -1,5 +1,5 @@
 var getTitle = function() {
-	return $('table:eq(2) b').text();
+  return $('table:eq(2) b').text();
 }
 
 var getCourse = function() {
@@ -10,5 +10,14 @@ var getTerm = function() {
   return getTitle().split('-')[1].trim();
 }
 
-console.log(getCourse());
-console.log(getTerm());
+var getSecretNumber = function() {
+  return "8811";
+}
+
+$('table:eq(3) tr:gt(2)').each(function() {
+  var secretNumber = $(this).find('td:eq(0)');
+  if (secretNumber.text() === getSecretNumber()) {
+    secretNumber.addClass('highlight');
+    secretNumber.siblings().addClass('highlight');
+  }
+});
